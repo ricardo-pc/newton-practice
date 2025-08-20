0152b7c4424983e262c474e4df17bbb1df03d8d1
Ricardo <ricardoperez@berkeley.edu>
2025-08-20 13:27:58 -0700

    Modified the Newton script.

--- newton.py
+++ newton.py
@@ -14,7 +14,24 @@
 # ---
 
 # %%
-def newton_method(x0, h, fx):
-    
+
+def deriv(f, x, eps = 1e-16):
+    # if x < 5:
+    #    raise RuntimeError("fake error")
+    return (f(x+eps) - f(x)) / eps
+
+
+def deriv2(f, x, eps = 1e-16):
+    return (deriv(f, x+eps, eps) - deriv(f, x, eps)) / eps
+
+
+def optimize(x0, f, tol =1e-4):
+    x_new = x0 - deriv(f, x0)/    deriv2(f, x0)
+  x = x0
+    while abs(x_new - x) < tol
+        x = x_new
+        x_new = x0 - deriv(f, x0) / deriv2(f, x0)
+    return {"x": x_new,
+            'value': f(x_new)}
 
 # %%
