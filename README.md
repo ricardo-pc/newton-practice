6747e94c2cd67274752107d94689d93a7eb695f7
Ricardo <ricardoperez@berkeley.edu>
2025-08-20 10:24:12 -0700

    Add basic implementation of Newton's method.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # newton-practice
 Berkeley Summer Prep - Computational Skills: Contains newton function in Python
+
+Wow, this file is edited now!
---
+++ newton.py
@@ -0,0 +1,20 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: py:percent
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.17.1
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+# %%
+def newton_method(x0, h, fx):
+    
+
+# %%
