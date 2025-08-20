83a5fe8c97ba942c3a79c0ca921628e749b5d1c5
Ricardo <ricardoperez@berkeley.edu>
2025-08-20 13:34:08 -0700

    fix syntax errors

--- newton.py
+++ newton.py
@@ -27,8 +27,8 @@ def deriv2(f, x, eps = 1e-16):
 
 def optimize(x0, f, tol =1e-4):
     x_new = x0 - deriv(f, x0)/    deriv2(f, x0)
-  x = x0
-    while abs(x_new - x) < tol
+    x = x0
+    while abs(x_new - x) < tol:
         x = x_new
         x_new = x0 - deriv(f, x0) / deriv2(f, x0)
     return {"x": x_new,
